627330378e973d1a6091e0dcda809f39f7c23705
Alex Vu <vuav@student.unimelb.edu.au>
2024-05-29 21:59:24 +1000

    so muh work idek what i did anymore

--- universal-script.js
+++ universal-script.js
@@ -20,54 +20,64 @@ function addOne(){
 const products = [
 
     {
-        shortName: "Sansai Bluetooth Speaker";
-        longName: "SANSAI 20W TWS BLUETOOTH SPEAKER WITH LED LIGHTS";
-        price: 84.70;
+        shortName: "Sansai Bluetooth Speaker",
+        longName: "SANSAI 20W TWS BLUETOOTH SPEAKER WITH LED LIGHTS",
+        price: 84.70
         
     },
     {
-        shortName: "Denon Bluetooth Speaker";
-        longName: "DENON HOME 150 WIRELESS HEOS BLUETOOTH SPEAKER";
-        price: 280.00;
+        shortName: "Denon Bluetooth Speaker",
+        longName: "DENON HOME 150 WIRELESS HEOS BLUETOOTH SPEAKER",
+        price: 280.00
     },
     {
-        shortName: "Mcdodo Cable";
-        longName: "MCDODO 100W 5A USB-C TO USB-C HIGH SPEED LEAD - 1.2M";
-        price: 24.00;
+        shortName: "Mcdodo Cable",
+        longName: "MCDODO 100W 5A USB-C TO USB-C HIGH SPEED LEAD - 1.2M",
+        price: 24.00
     },
     {
-        shortName: "Mcdodo Charger";
-        longName: "MCDODO 3A RIGHT ANGLE USB TO RIGHT ANGLE LIGHTNING LEAD - 3M";
-        price: 24.00;
+        shortName: "Mcdodo Charger",
+        longName: "MCDODO 3A RIGHT ANGLE USB TO RIGHT ANGLE LIGHTNING LEAD - 3M",
+        price: 24.00
     },
     {
-        shortName: "Tp-Link Port";
-        longName: "TP-LINK 5 PORT NETWORK SWITCH HUB";
-        price: 22.00;
+        shortName: "Tp-Link Port",
+        longName: "TP-LINK 5 PORT NETWORK SWITCH HUB",
+        price: 22.00
     },
     {
-        shortName: "Sansai Remote";
-        longName: "SANSAI 8 IN 1 UNIVERSAL REMOTE CONTROL WITH LEARNING FUNCTION";
-        price: 26.95;
+        shortName: "Sansai Remote",
+        longName: "SANSAI 8 IN 1 UNIVERSAL REMOTE CONTROL WITH LEARNING FUNCTION",
+        price: 26.95
     },
     {
-        shortName: "Sonken Microphone";
-        longName: "SONKEN 2CH WIRELESS MICROPHONE RECEIVER WITH 2 MICS INCLUDED";
-        price: 400.00;
+        shortName: "Sonken Microphone",
+        longName: "SONKEN 2CH WIRELESS MICROPHONE RECEIVER WITH 2 MICS INCLUDED",
+        price: 400.00
     },
     {
-        shortName: "Koss Headphones";
-        longName: "KOSS BUDGET ON EAR STEREO HEADPHONES - BLACK";
-        price: 70.00;
+        shortName: "Koss Headphones",
+        longName: "KOSS BUDGET ON EAR STEREO HEADPHONES - BLACK",
+        price: 70.00
     },
     {
-        shortName: "Westec Headset";
-        longName: "WESTEC KIDS 85DB RECHARGEABLE BLUETOOTH HEADPHONES - PURPLE";
-        price: 59.00;
+        shortName: "Westec Headset",
+        longName: "WESTEC KIDS 85DB RECHARGEABLE BLUETOOTH HEADPHONES - PURPLE",
+        price: 59.00
     },
     {
-        shortName: "Mcdodo Power Bank";
-        longName: "MCDODO 10,000MAH POWER BANK - 2x QC3.0 USB + 1x PD TYPE-C OUTPUTS";
-        price: 39.00;
+        shortName: "Mcdodo Power Bank",
+        longName: "MCDODO 10,000MAH POWER BANK - 2x QC3.0 USB + 1x PD TYPE-C OUTPUTS",
+        price: 39.00
     }
-]
+];
+
+function hideProductsPageElements(){
+    items = document.querySelector('.items-container');
+    search = document.querySelector('.filter-bar-search');
+    sort = document.querySelector('.filter-bar-sort');
+
+    items.style.visibility = 'hidden';
+    search.style.visibility = 'hidden';
+    sort.style.visibility = 'hidden';
+};
